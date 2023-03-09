3b49d685680139775d188e18c0c10a8daa661f07
qnater <quentin.nater@gmail.com>
2023-03-09 15:22:00 +0100

    display a sampled graph

--- explore/exploration_graph.py
+++ explore/exploration_graph.py
@@ -1,14 +1,36 @@
-import math
-
 import networkx as nx
-import matplotlib as mpl
+import random
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+
 
 class explore_graph:
 
-    def explore_simple_graph(myGraph):
+    # Quentin Nater
+    # Display a simple graph sample with a file
+    def display_simple_graph(myGraph):
 
         nx.draw(myGraph, with_labels=True)
         plt.show()
 
         return myGraph
+
+    # Quentin Nater
+    # Display a simple graph sample with a file
+    def display_simple_file(file_name):
+        myGraph = nx.read_edgelist(file_name, create_using=nx.DiGraph(), nodetype=int)
+
+        # number of nodes to sample
+        num_nodes = 1000
+
+        # randomly sample nodes
+        nodes_list = list(myGraph.nodes())
+        sampled_nodes = random.sample(nodes_list, num_nodes)
+
+        # create a subgraph with the sampled nodes and all their edges
+        sampled_graph = myGraph.subgraph(sampled_nodes)
+
+        # draw the sampled graph
+        nx.draw(sampled_graph, with_labels=True)
+        plt.show()
--- project_graph.py
+++ project_graph.py
@@ -1,8 +1,6 @@
-import math
-
-import networkx as nx
 import matplotlib as mpl
-import matplotlib.pyplot as plt
+import networkx as nx
+
 from explore.exploration_graph import explore_graph as eg
 
 # QUENTIN NATER - 01.03.2023 - ASS 1 - EX 2
@@ -13,8 +11,12 @@ if __name__ == '__main__':
 
     mpl.use('TkAgg')  # without it, cannot run my plots (maybe personal)
 
-    myGraph = nx.Graph()
-    myGraph.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
-    myGraph.add_edges_from([(1, 2), (1, 4), (1, 7), (2, 5), (2, 6), (3, 5), (3, 6)])
+    tag = "prod"  # prod or test
 
-    eg.explore_simple_graph(myGraph)
+    if tag == "test":
+        myGraph = nx.Graph()
+        myGraph.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
+        myGraph.add_edges_from([(1, 2), (1, 4), (1, 7), (2, 5), (2, 6), (3, 5), (3, 6)])
+        eg.display_simple_graph(myGraph)
+    elif tag == "prod":
+        eg.display_simple_file("./dataset/roadNet-CA.txt.gz")
