93aa49eb62030b73493992709774cd39eaccf301
qnater <quentin.nater@gmail.com>
2023-03-15 13:38:39 +0100

    display of graphs

--- project_graph.py
+++ project_graph.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     mpl.use('TkAgg')  # without it, cannot run my plots (maybe personal)
 
-    tag = "prod"  # prod or test
+    tag, display = "prod", False  # prod or test
 
     if tag == "test":
         myGraph = nx.Graph()
@@ -20,5 +20,5 @@ if __name__ == '__main__':
         myGraph.add_edges_from([(1, 2), (1, 4), (1, 7), (2, 5), (2, 6), (3, 5), (3, 6)])
         vg.display_simple_graph(myGraph)
     elif tag == "prod":
-        graph = eg.construct_simple_file("./dataset/amazon-meta.txt", 100000)
+        graph = eg.construct_simple_file("./dataset/amazon-meta.txt", 1000)
         vg.display_simple_graph(graph)
--- visualization/visualization_graph.py
+++ visualization/visualization_graph.py
@@ -8,16 +8,17 @@ class visualization_graph:
 
     # Quentin Nater
     # Display a simple graph sample with a file
-    def display_simple_graph(myGraph):
+    def display_simple_graph(myGraph, display=True):
 
-        nx.draw(myGraph, with_labels=True)
-        plt.show()
+        if display:
+           nx.draw(myGraph, with_labels=True)
+           plt.show()
 
         return myGraph
 
     # Quentin Nater
     # Display a simple graph sample with a file - test
-    def display_sampled_file(file_name):
+    def display_sampled_file(file_name, display=True):
         myGraph = nx.read_edgelist(file_name, create_using=nx.DiGraph(), nodetype=int)
 
         # number of nodes to sample
@@ -30,13 +31,14 @@ class visualization_graph:
         # create a subgraph with the sampled nodes and all their edges
         sampled_graph = myGraph.subgraph(sampled_nodes)
 
-        # draw the sampled graph
-        nx.draw(sampled_graph, with_labels=True)
-        plt.show()
+        if display:
+            # draw the sampled graph
+            nx.draw(sampled_graph, with_labels=True)
+            plt.show()
 
     # Quentin Nater
     # Display a simple graph sample with a file - test
-    def display_sampled_graph(myGraph):
+    def display_sampled_graph(myGraph, display=True):
         # number of nodes to sample
         num_nodes = 1000
 
@@ -47,9 +49,10 @@ class visualization_graph:
         # create a subgraph with the sampled nodes and all their edges
         sampled_graph = myGraph.subgraph(sampled_nodes)
 
-        # draw the sampled graph
-        nx.draw(sampled_graph, with_labels=True)
-        plt.show()
+        if display:
+            # draw the sampled graph
+            nx.draw(sampled_graph, with_labels=True)
+            plt.show()
 
 
 
